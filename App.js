202aa9523f10eed244d2ba555a96c0b7ef32dd2c
hyoloui <tmdgy9272@gmail.com>
2023-01-03 20:42:23 +0900

    📌 practice: native stack
    리엑트 네비게이션 > 네이티브 스택 연습하기
    
    by @hyoloui

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
+// react-navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+const Stack = createNativeStackNavigator();
+
+const Zero = ({ navigation: { navigate } }) => {
+    return (
+        <TouchableOpacity onPress={() => navigate("one")}>
+            <Text>제로</Text>
+        </TouchableOpacity>
+    );
+};
+const One = ({ navigation: { navigate } }) => {
+    return (
+        <TouchableOpacity onPress={() => navigate("two")}>
+            <Text>원</Text>
+        </TouchableOpacity>
+    );
+};
+const Two = ({ navigation: { goBack } }) => {
+    return (
+        <TouchableOpacity onPress={() => goBack()}>
+            <Text>투</Text>
+        </TouchableOpacity>
+    );
+};
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text>Open up App.</Text>
-            <StatusBar style="auto" />
-        </View>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="zero" component={Zero}></Stack.Screen>
+                <Stack.Screen name="one" component={One}></Stack.Screen>
+                <Stack.Screen name="two" component={Two}></Stack.Screen>
+            </Stack.Navigator>
+        </NavigationContainer>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-});
