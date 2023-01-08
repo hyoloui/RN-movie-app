78500c97584fb7c4d3ca1e7ce332177b2813ee2e
hyoloui <tmdgy9272@gmail.com>
2023-01-08 17:28:49 +0900

    🌱 update: App.js
    react-query update for App.js
    
    by @hyoloui

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
 import { ThemeProvider } from "@emotion/react";
 import {
-    DarkTheme,
-    DefaultTheme,
-    NavigationContainer,
+  DarkTheme,
+  DefaultTheme,
+  NavigationContainer,
 } from "@react-navigation/native";
 import { useColorScheme } from "react-native";
 import Root from "./navigation/Root";
 import { darkTheme, lightTheme } from "./theme";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 export default function App() {
-    const isDark = useColorScheme() === "dark";
-    console.log("🚀 ~ file: App.js:11 ~ App ~ isDark", isDark);
+  const isDark = useColorScheme() === "dark";
+  console.log("🚀 ~ file: App.js:11 ~ App ~ isDark", isDark);
 
-    return (
-        <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
-            <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
-                <Root />
-            </NavigationContainer>
-        </ThemeProvider>
-    );
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+        <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
+          <Root />
+        </NavigationContainer>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
 }
